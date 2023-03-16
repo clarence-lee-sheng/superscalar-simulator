003584d6334762dccddf3044b065e44a862a7593
Reckonzz <cl.sheng34@gmail.com>
2023-03-16 22:04:08 +0000

    updated InstructionQueue to be a fetch buffer

---
+++ hardware/FetchBuffer.py
@@ -0,0 +1,25 @@
+class FetchBuffer: 
+    def __init__(self, size):
+        self.size = size
+        self.queue = []
+
+    def is_full(self):
+        return len(self.queue) == self.size
+    
+    def enqueue(self, instruction):
+        if len(self.queue) < self.size:
+            self.queue.append(instruction)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            return self.queue.pop(0)
+        
+    def __str__(self):
+        return str(self.queue)
+    
+    def __iter__(self):
+        for ins in self.queue:
+            yield ins
+
+
+    
