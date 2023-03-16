79155713e4117c27fbe62b1ed37ad3df07d33721
Reckonzz <cl.sheng34@gmail.com>
2023-03-16 22:02:51 +0000

    updated simple processor to match updated Instruction Class

--- simple.py
+++ simple.py
@@ -2,24 +2,30 @@ from hardware.RegisterFile import RegisterFile
 from hardware.Memory import Memory 
 from hardware.CPU import CPU
 from software.Assembler import Assembler 
+from copy import deepcopy
 import re
-
 import os
 
-reg_size = 32 
-mem_size = 4096
-registers = RegisterFile(reg_size=32)
-memory = Memory(mem_size=mem_size)
-assembler = Assembler() 
+config = { 
+    "rob_buffer_size": 4,
+    "iq_buffer_size": 4,
+    "lsq_buffer_size": 4,
+    "rs_buffer_size": 4,
+    "n_gprs": 32,
+    "mem_size": 4096,
+    "register_file_params": {}
+}
+
 
 class Simple(CPU):
-    def __init__(self,registers,memory, assembler):
-        super().__init__(registers, memory, assembler) 
-        self.statistics = {
-            "instructions_count": 0,
-            "load_count": 0,
-            "store_count": 0
-        }
+    def __init__(self,config):
+        super().__init__(config) 
+        # self.statistics = {
+        #     "instructions_count": 0,
+        #     "load_count": 0,
+        #     "store_count": 0, 
+        #     "cycles": 0 
+        # }
     def fetch(self): 
         if not self.program: 
             print("no instructions to fetch as there is no program")
@@ -27,72 +33,75 @@ class Simple(CPU):
         else: 
             instruction = self.program[self.pc]
             self.statistics["instructions_count"] += 1
-            opcode = instruction[0]
+            opcode = instruction.opcode
             if opcode == "lw" or opcode == "li" or opcode == "la":
                 self.statistics["load_count"] += 1
             if opcode == "sw":
                 self.statistics["store_count"] += 1
             return instruction
     def decode(self, instruction): 
-        instruction = instruction.copy()
-        opcode = instruction[0]
+        print(instruction)
+        print(self.registers.physical_registers)
+        instruction = deepcopy(instruction)
+        opcode = instruction.opcode
+        operands = instruction.operands
         if opcode == "add": 
-            instruction[2] = self.registers[instruction[2]]
-            instruction[3] = self.registers[instruction[3]]
+            instruction.operands[1] = self.registers[instruction.operands[1]]
+            instruction.operands[2] = self.registers[instruction.operands[2]]
         if opcode == "li": 
-            instruction[2] = int(instruction[2])
+            instruction.operands[1] = int(instruction.operands[1])
         if opcode == "lw":
-            reg = re.search(r"(?<=\()(.*?)(?=\))", instruction[2]).group()
-            offset = re.search(r".*(?=\()", instruction[2]).group()
-            instruction[2] = self.registers[reg] + int(offset)
+            reg = re.search(r"(?<=\()(.*?)(?=\))", instruction.operands[1]).group()
+            offset = re.search(r".*(?=\()", instruction.operands[1]).group()
+            instruction.operands[1] = self.registers[reg] + int(offset)
         if opcode == "sw":
-            reg = re.search(r"(?<=\()(.*?)(?=\))", instruction[2]).group()
-            offset = re.search(r".*(?=\()", instruction[2]).group()
-            instruction[1] = self.registers[instruction[1]]
-            instruction[2] = self.registers[reg] + int(offset)
+            reg = re.search(r"(?<=\()(.*?)(?=\))", instruction.operands[1]).group()
+            offset = re.search(r".*(?=\()", instruction.operands[1]).group()
+            instruction.operands[0] = self.registers[instruction.operands[0]]
+            instruction.operands[1] = self.registers[reg] + int(offset)
         if opcode == "addi":
-            instruction[2] = int(instruction[2])
+            instruction.operands[1] = int(instruction.operands[1])
         return instruction
     def execute(self, decoded): 
         intermediate = dict()
-        opcode = decoded[0]
+        opcode = decoded.opcode
 
         if opcode == "add":
-            intermediate["value"] = decoded[2] + decoded[3]
+            print(decoded.operands)
+            intermediate["value"] = decoded.operands[1] + decoded.operands[2]
             intermediate["type"] = "register"
-            intermediate["id"] = decoded[1]
+            intermediate["id"] = decoded.operands[0]
         elif opcode == "li":
-            intermediate["value"] = decoded[2]
+            intermediate["value"] = decoded.operands[1]
             intermediate["type"] = "register"
-            intermediate["id"] = decoded[1]
+            intermediate["id"] = decoded.operands[0]
         elif opcode == "la": 
-            intermediate["value"] = decoded[2]
+            intermediate["value"] = decoded.operands[1]
             intermediate["type"] = "register"
-            intermediate["id"] = decoded[1]
+            intermediate["id"] = decoded.operands[0]
         elif opcode == "lw":
-            intermediate["value"] = self.memory[decoded[2]]
+            intermediate["value"] = self.memory[decoded.operands[1]]
             intermediate["type"] = "register"
-            intermediate["id"] = decoded[1]
+            intermediate["id"] = decoded.operands[0]
         elif opcode == "sw":
-            print(decoded)
-            intermediate["value"] = decoded[1]
+            intermediate["value"] = decoded.operands[0]
             intermediate["type"] = "memory"
-            intermediate["id"] = decoded[2]
+            intermediate["id"] = decoded.operands[1]
         elif opcode == "addi":
-            intermediate["value"] = self.registers[decoded[1]] + decoded[2]
+            intermediate["value"] = self.registers[decoded.operands[0]] + decoded.operands[1]
             intermediate["type"] = "register"
-            intermediate["id"] = decoded[1]
+            intermediate["id"] = decoded.operands[0]
         elif opcode == "ecall":
             intermediate["value"] = None 
             intermediate["type"] = "sys"
             intermediate["id"] = None 
         elif opcode == "j": 
             intermediate["type"] = "pc"
-            intermediate["value"] = decoded[1]
+            intermediate["value"] = decoded.operands[0]
         elif opcode == "beq":
-            if self.registers[decoded[1]] == self.registers[decoded[2]]:
+            if self.registers[decoded.operands[0]] == self.registers[decoded.operands[1]]:
                 intermediate["type"] = "pc"
-                intermediate["value"] = decoded[3]
+                intermediate["value"] = decoded.operands[2]
             else:
                 intermediate["type"] = "pc"
                 intermediate["value"] = self.pc + 1
@@ -123,7 +132,11 @@ class Simple(CPU):
         pass
     def run(self, program): 
         self.program = self.assembler.assemble(program, self)
+        for instruction in self.program: 
+            print(instruction)
+        print(self.program)
         while True: 
+            self.statistics["cycles"] += 5
             instruction = self.fetch()
             decoded = self.decode(instruction) 
             intermediate = self.execute(decoded)
@@ -132,11 +145,14 @@ class Simple(CPU):
             if self.pc >= len(self.program):
                 break
         print("Number of instructions executed: ", self.statistics["instructions_count"])
+        print("Number of cycles: ", self.statistics["cycles"])
+        print("Instruction per cycle: ", self.statistics["instructions_count"]/self.statistics["cycles"])
+        print("Cycles per instruction: ", self.statistics["cycles"]/self.statistics["instructions_count"])
         print("Number of stores executed: ", self.statistics["store_count"])
         print("Number of loads executed: ", self.statistics["load_count"])
 
-cpu = Simple(registers, memory, assembler)
-cpu.run(os.path.join(os.getcwd(), "benchmark\\vec_addition.s"))
-print(registers)
+cpu = Simple(config)
+cpu.run(os.path.join(os.getcwd(), "programs\\vec_addition.s"))
+print(cpu.registers)
 print(cpu.memory[:40])
 
