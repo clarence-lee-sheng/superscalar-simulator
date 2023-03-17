e8234d1455e4030174db4d4386a54a7e95d7f87c
Reckonzz <cl.sheng34@gmail.com>
2023-03-17 22:23:25 +0000

    added enqueue and commit methods

--- hardware/ReorderBuffer.py
+++ hardware/ReorderBuffer.py
@@ -1,6 +1,7 @@
 class ReorderBufferEntry: 
     def __init__(self): 
-        self.reg = None
+        self.type = None 
+        self.dest = None 
         self.value = None
         self.ready = False
 
@@ -10,4 +11,27 @@ class ReorderBuffer:
         self.reorder_buffer = [ReorderBufferEntry() for i in range(reorder_buffer_size)]
         self.head = 0 # points to the next available entry e.g. issue
         self.tail = 0 # points to the oldest entry e.g. commit
-        self.size = 0
+
+    def is_full(self): 
+        if self.head % self.reorder_buffer_size == self.tail % self.reorder_buffer_size: 
+            return True
+        return self.size == self.reorder_buffer_size
+    
+    def enqueue(self, type, dest, value): 
+        if self.is_full(): 
+            print("buffer full, stalling")
+            return False
+        self.reorder_buffer[self.head].type = type
+        self.reorder_buffer[self.head].dest = dest
+        self.reorder_buffer[self.head].value = value
+        self.reorder_buffer[self.head].ready = False
+        self.tail += 1
+        return True
+
+    def commit(self): 
+        if self.head == self.tail: 
+            raise Exception("Nothing to commit")
+        if self.reorder_buffer[self.tail].ready:
+            self.reorder_buffer[self.tail] = ReorderBufferEntry()
+            self.head += 1
+
