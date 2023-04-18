00f4fcce3b18c0e415cac129938de4c77fc15fb0
Reckonzz <cl.sheng34@gmail.com>
2023-04-18 21:29:40 +0100

    updated Issue Queue to include issue slot and issue function

--- hardware/IssueQueue.py
+++ hardware/IssueQueue.py
@@ -1,26 +1,17 @@
-class ReservationStationInstance:
-    def __init__(self, name, op, vj, vk, qj, qk, a):
-        self.name = name
-        self.op = op
-        self.vj = vj
-        self.vk = vk
-        self.qj = qj
-        self.qk = qk
-        self.a = a
-        self.busy = False
-
-
-class IssueEntry: 
-    def __init__(self, instruction): 
-        self.op = instruction.micro_op 
-        self.vj = None 
-        self.vk = None 
-        self.qj = 
-        self.instruction = instruction 
-        self.rs1 = instruction.src1
-        self.rs2 = instruction.src2
-        self.requesting = False
+class IssueSlot: 
+    def __init__(self, opcode, dest, src1, src1_ready, src2, src2_ready): 
+        # this entry is based on the issue slot according to the documentation here: https://docs.boom-core.org/en/latest/sections/issue-units.html 
+        self.opcode = opcode
+        self.dest = dest
+        self.src1 = src1 
+        self.src1_ready = src1_ready
+        self.src2 = src2
+        self.src2_ready = src2_ready
+        # requesting is set to true when the instruction is ready to be issued, under issue slot chapter: https://docs.boom-core.org/en/latest/sections/issue-units.html
+        self.requesting = src1_ready and src2_ready
     
+    def __str__(self):
+        return f"opcode: {self.opcode} dest: {self.dest} src1: {self.src1} src1_ready: {self.src1_ready} src2: {self.src2} src2_ready: {self.src2_ready} requesting: {self.requesting}"
 
 class IssueQueue: 
     def __init__(self, size, execution_units, register_file):
@@ -36,25 +27,28 @@ class IssueQueue:
     def is_empty(self):
         return len(self.queue) == 0
     
-    def enqueue(self, instruction):
+    def enqueue(self, issue_entry):
         if len(self.queue) < self.size:
-            self.queue.append(instruction)
+            self.queue.append(issue_entry)
     
     def dequeue(self):
         if len(self.queue) > 0:
             return self.queue.pop(0)
         
     def issue(self): 
-        for instruction in self.queue: 
-            if instruction.requesting = True
-
         for execution_unit in self.execution_units: 
             if execution_unit.busy:
                 continue 
             for i, issue_entry in enumerate(self.queue):
+                print(issue_entry)
                 if issue_entry.requesting:
+                    print("requesting")
                     execution_unit.allocate(issue_entry)
+                    self.queue.pop(i)
+                break
                      
+    def __str__(self):
+        return str([str(i) for i in self.queue])
 
 
 
