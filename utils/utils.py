7f1475c89a5b80ec2bd70be7641fa08bfc1d7de4
Reckonzz <cl.sheng34@gmail.com>
2023-04-22 20:09:03 +0100

    fixed bug to int offset

--- utils/utils.py
+++ utils/utils.py
@@ -4,9 +4,15 @@ def extract_reg_and_offset(operand):
     reg = re.search(r"(?<=\()(.*?)(?=\))", str(operand))
     if reg: 
         reg = reg.group(0)
-        offset = re.search(r".*(?=\()", operand).group()
+        offset = int(re.search(r".*(?=\()", operand).group())
     else: 
         reg = operand
         offset = None
     
     return reg, offset
+
+UUID = 0
+def getUUID():
+    global UUID
+    UUID += 1
+    return UUID
