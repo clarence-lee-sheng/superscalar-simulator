2bbfa776fba293a8afb50bd96dc30b0fb7d4d230
Reckonzz <cl.sheng34@gmail.com>
2023-05-01 21:58:43 +0100

    added convolution assembly program

--- hardware/ReorderBuffer.py
+++ hardware/ReorderBuffer.py
@@ -13,7 +13,7 @@ class ReorderBufferEntry:
         self.branch_mask = None 
     
     def __str__(self): 
-        return f"pc: {self.pc} type: {self.type} dest: {self.dest} value: {self.value} busy: {self.busy} valid: {self.valid} stale: {self.stale} stale_physical_destination: {self.stale_physical_destination} exception: {self.exception}"
+        return f"uuid: {self.uuid} pc: {self.pc} type: {self.type} dest: {self.dest} value: {self.value} ready: {self.ready} valid: {self.valid} stale: {self.stale} stale_physical_destination: {self.stale_physical_destination} exception: {self.exception}\n"
 
 # everytime a new instruction is met, rename and store in the reorder buffer 
 
@@ -23,11 +23,11 @@ class ReorderBuffer:
         self.reorder_buffer_size = reorder_buffer_size
         self.reorder_buffer = [ReorderBufferEntry() for i in range(reorder_buffer_size)]
         self.head = 0 # points to the next available entry e.g. issue
-        self.tail = 1 # points to the oldest entry e.g. commit
+        self.tail = 0 # points to the oldest entry e.g. commit
         self.register_file = register_file
 
     def is_full(self): 
-        if self.head % self.reorder_buffer_size == self.tail % self.reorder_buffer_size: 
+        if (self.tail + 1) % self.reorder_buffer_size == self.head % self.reorder_buffer_size: 
             return True
         else: 
             return False
@@ -36,7 +36,7 @@ class ReorderBuffer:
     def enqueue(self, uuid, pc, type, dest, value, stale_physical_destination, branch_mask, ready=False): 
         if self.is_full(): 
             raise Exception
-            return False
+
         self.reorder_buffer[self.tail].uuid = uuid
         self.reorder_buffer[self.tail].pc = pc
         self.reorder_buffer[self.tail].type = type
@@ -47,46 +47,96 @@ class ReorderBuffer:
         self.reorder_buffer[self.tail].stale_physical_destination = stale_physical_destination
         self.reorder_buffer[self.tail].branch_mask = branch_mask
 
-        if self.tail == len(self.reorder_buffer) - 1: 
-            self.tail = 0 
-        else: 
-            self.tail += 1
+        self.tail = (self.tail + 1) % self.reorder_buffer_size
+
         return True
 
     def commit(self): 
         if self.head == self.tail: 
             return 
+        # print("head: ", self.head, "tail: ", self.tail)
         while self.reorder_buffer[self.head].ready:
-            print("COMMITTING")
+            # if self.reorder_buffer[self.head].uuid == 726:
+            #     raise Exception("726")
+        
+            # print("COMMITTING", self.cpu.program[self.reorder_buffer[self.head].pc])
             if self.reorder_buffer[self.head].value == "exit":
                 self.cpu.exit = True 
             entry = self.reorder_buffer[self.head]
             if entry.stale_physical_destination is not None: 
-                self.register_file.committed_map_table.free(entry.stale_physical_destination)
+                self.cpu.registers.free(entry.stale_physical_destination)
+                reg = self.cpu.program[entry.pc].operands[0]
+                self.cpu.registers.committed_map_table[reg] = entry.dest
             # self.register_file.committed_map_table.free(entry.stale_physical_destination)
+            if self.cpu.registers.snapshots.get(entry.uuid,False):
+                self.cpu.registers.snapshots.pop(entry.uuid)
+
+            if self.cpu.program[self.reorder_buffer[self.head].pc].opcode == "sw":
+                for entry in self.cpu.LSU.store_queue:
+                    print("comparing uuid: ",entry.uuid, self.reorder_buffer[self.head].uuid)
+                    if entry.uuid == self.reorder_buffer[self.head].uuid:
+                        entry.valid = True
+
+            self.cpu.branch_predictor.fetch_target_queue.pop(entry.uuid, None)
             self.reorder_buffer[self.tail] = ReorderBufferEntry()
             self.head += 1
+            self.head = self.head % self.reorder_buffer_size
+            self.cpu.statistics["instructions_count"] +=1
 
     def flush(self, uuid):
         # flush all entries after uuid
-        for i, entry in enumerate(self.reorder_buffer):
-            if entry.uuid == uuid:
-                break 
-        self.tail = i
+        tail = self.tail 
+        entry = self.reorder_buffer[tail]
+        flag = True
+        while uuid != entry.uuid:
+            if self.cpu.registers.snapshots.get(entry.uuid,False):
+                # print("POPPING", entry.uuid)
+                self.cpu.registers.snapshots.pop(entry.uuid)
+            if entry.pc: 
+                if self.cpu.program[entry.pc].opcode == "sw":
+                    for s in self.cpu.LSU.store_queue:
+                        self.cpu.LSU.store_queue.remove(s)
+
+            tail = (tail - 1) % self.reorder_buffer_size
+            entry = self.reorder_buffer[tail]
+        if self.cpu.registers.snapshots.get(entry.uuid,False):
+            self.cpu.registers.snapshots.pop(entry.uuid)
+        # if self.cpu.registers.snapshots.get(entry.uuid,False):
+        #     self.cpu.registers.snapshots.pop(entry.uuid)
+
+        # if self.tail > self.head: 
+        #     for i, entry in enumerate(self.reorder_buffer):
+        #         if entry.uuid == 99:
+        #             print("flushing: ", entry.uuid, entry.pc, entry.type, entry.dest, entry.value, entry.ready, entry.stale_physical_destination, entry.branch_mask)
+        #             print(self)
+
+        #         if entry.uuid == 116: 
+        #             print("flushing 116")
+                
+        #         if entry.uuid == uuid:
+        #             print("FLUSHING", entry.uuid)
+        #             break 
+        #         if self.cpu.registers.snapshots.get(entry.uuid,False):
+        #             # print("POPPING", entry.uuid)
+        #             self.cpu.registers.snapshots.pop(entry.uuid)
+        self.tail = tail 
         # self.tail = self.head
 
         # rewrite stale physical destination to RAT 
 
     def update(self, uuid, is_exception, commit): 
-        for entry in self.reorder_buffer: 
+        for i, entry in enumerate(self.reorder_buffer): 
             if entry.uuid == uuid: 
                 if is_exception: 
-                    entry.exception = True
+                    self.reorder_buffer[i].exception = True
                 if commit: 
-                    entry.busy = False
+                    self.reorder_buffer[i].ready = True
 
 
     def __str__(self): 
-        return str([str(rob_entry) for rob_entry in self.reorder_buffer])
+        output = "self.head: " + str(self.head) + " self.tail: " + str(self.tail) + "\n"
+        for rob_entry in self.reorder_buffer: 
+            output += str(rob_entry) + "\n"
+        return output
 
 
---
+++ programs/convolution.s
@@ -0,0 +1,89 @@
+.section .data
+A: .word 1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17
+B: .word 1,2,0,1
+C: .word 0,0,0,0,0,0,0,0,0
+
+.text
+.global _start 
+
+_start: 
+    li t0, 0
+    li t1, 0
+    # setting matrix A dimensions to i x k and matrix B dimensions to k x j while C dimensions are i x j
+
+    # matrix dimension 
+    li s0, 4
+    li s1, 4
+    li s2, 2 
+    la a0, A
+    la a1, B
+    la a2, C 
+
+    lw s3, 0(a1)
+    lw s4, 1(a1)
+    lw s5, 2(a1)
+    lw s6, 3(a1)
+
+    # stride 
+    li s7 1 
+
+    # get iteration counts 
+    div t2, s0, s7
+    sub t2, t2, s2
+    addi t2, t2, 1
+    div t3, s2, s7
+    sub t3, t3, s2
+    addi t3, t3, 1
+
+loop1:
+    bge t0, t2, done 
+    li t1, 0
+    j loop2 
+
+loop2:
+    bge t1, t3, endloop2
+    li s11, 0 
+    mul t4, t0, s0 
+    add t4, t4, t1 
+    lw t4, 0(t4)
+    mul t4, t4, s3
+
+    addi t5, t0, 1 
+    mul t5, t5, s0 
+    add t5, t5, t1 
+    lw t5, 0(t5)
+    mul t5, t5, s5 
+
+    add t4, t4, t5 
+
+    mul t4, t0, s0 
+    add t4, t4, t1
+    addi t4, t4, 1 
+    lw t4, 0(t4)
+    mul t4, t4, s4
+
+    addi t5, t0, 1 
+    mul t5, t5, s0 
+    add t5, t5, t1 
+    addi t5, t5, 1 
+    lw t5, 0(t5)
+    mul t5, t5, s6
+
+    add t6, t4, t5
+
+    add s11, t4, t6 
+
+    mul t4, t0, t2 
+    add t4, t4, t1 
+    add t4, t4, a2
+    sw s11, 0(t4)
+    
+    addi t1, t1, 1
+    j loop2
+
+endloop2: 
+  addi t0, t0, 1
+  j loop1
+
+done: 
+  ecall 
