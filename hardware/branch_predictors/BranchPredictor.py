dce4925466792f8bf60463f47f14e4cb96ec9d09
Reckonzz <cl.sheng34@gmail.com>
2023-05-01 21:59:43 +0100

    fixed check_prediction

--- hardware/branch_predictors/BranchPredictor.py
+++ hardware/branch_predictors/BranchPredictor.py
@@ -1,44 +1,53 @@
+from collections import deque
+import copy
 class BranchPredictor: 
     def __init__(self, cpu):
+        n_global_history_register = cpu.config["branch_predictor"]["global_history_register_size"]
         self.cpu = cpu
         self.branch_history_buffer = {}
         self.branch_target_buffer = {}
-        self.global_history_register = {}
+        self.global_history_register = deque([0 for i in range(n_global_history_register)],maxlen=n_global_history_register)
         self.fetch_target_queue = {}
         self.snapshots = {}
     def predict_taken(self, branch_pc, pc): 
         pass 
     def predict(self, pc, uuid): 
+        self.snapshots[uuid] = {
+            "branch_history_buffer": copy.deepcopy(self.branch_history_buffer),
+            "global_history_register": copy.deepcopy(self.global_history_register),
+        }
         if pc in self.branch_target_buffer.keys(): 
-            print("IS IN BRANCH TARGET BUFFER", pc)
+            # print("IS IN BRANCH TARGET BUFFER", pc)
+            
             taken = self.predict_taken(pc, uuid)  
+            
             if taken: 
                 target_pc = self.branch_target_buffer[pc]
             else: 
                 target_pc =  pc + 1
             self.fetch_target_queue[uuid] = taken
         else: 
+            taken = False
             target_pc = pc + 1
             self.fetch_target_queue[uuid] = False
         
+        if taken: 
+            self.global_history_register.appendleft(1)
+        else:
+            self.global_history_register.appendleft(0)
         return target_pc
-    def snapshot(self, pc, branch_tag): 
-        self.snapshots[branch_tag] = {
-            "branch_history_buffer": self.branch_history_buffer,
-            "global_history_register": self.global_history_register,
-        }
-    
+
     def check_prediction(self, prediction, branch_tag, uuid, pc): 
-        if uuid in self.fetch_target_queue.keys(): 
-            pred = self.fetch_target_queue[uuid]
-        else: 
-            pred = pc + 1
+        if pc in self.branch_target_buffer.keys(): 
+            taken_pc = self.branch_target_buffer[pc]
+        not_taken_pc = pc + 1
+        
+        pred_taken = self.fetch_target_queue[uuid]
 
-
+        pred = taken_pc if pred_taken else not_taken_pc
         if pred == prediction:
             return True
         else: 
-            self.branch_history_buffer = self.snapshots[branch_tag]["branch_history_buffer"]
-            self.global_history_register = self.snapshots[branch_tag]["global_history_register"]
+            self.global_history_register.appendleft(1 if pred != pc+1 else 0)
             return False
         
