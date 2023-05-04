00b43c03127d79da9e18a8a4c7a2ac71fb4bb767
Reckonzz <cl.sheng34@gmail.com>
2023-05-04 14:44:40 +0100

    updated vec addition

--- programs/vec_addition.s
+++ programs/vec_addition.s
@@ -1,29 +1,30 @@
 .section .data
-a: .word 1,2,3,4,5,6,7,8,9,10 
-b: .word 1,2,3,4,5,6,7,8,9,10
-c: .word 0,0,0,0,0,0,0,0,0,0 
+a: .word 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+b: .word 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+c: .word 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 
 
 .text
 .global _start 
 
 _start: 
-    li s0, 0 
-    li s1, 10
+    # test
+    li s5, 0 
+    li s1, 20
     la s2, a
     la s3, b
     la s4, c  
     
 loop: 
-    beq s0, s1, done 
-    add t1, s2, s0
-    add t2, s3, s0
-    add t3, s4, s0
+    beq s5, s1, done 
+    add t1, s2, s5
+    add t2, s3, s5
+    add t3, s4, s5
     lw t1, 0(t1)
     lw t2, 0(t2)
     add t4, t1, t2 
     sw t4, 0(t3) 
-    addi s0, s0, 1
+    addi s5, s5, 1
     j loop
 
 done: 
