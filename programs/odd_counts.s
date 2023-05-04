838ead5c94f9eb7e6484d2138ecaaf79e9454d82
Reckonzz <cl.sheng34@gmail.com>
2023-05-04 14:43:13 +0100

    updated odd_counts

--- programs/odd_counts.s
+++ programs/odd_counts.s
@@ -7,21 +7,22 @@ result: .word 0
 _start: 
     # test
     li s5, 0 
-    li s1, 50
+    li s1, 400
     li s2, 0 
+    li s4, 1.0
     la s3, result
     
 loop: 
     beq s5, s1, done 
-    remw t1, s5, 2 
-    bne t1, zero, add_count
-    addi s5, s5, 1
+    remw t1, s5, 2
+    beq, t1, s4, add_count
+    add s5, s5, s4
     j loop
 
 add_count: 
     addi s2, s2, 1
     sw s2, 0(s3)
-    addi s5, s5, 1
+    add s5, s5, s4
     j loop 
 
 done: 
