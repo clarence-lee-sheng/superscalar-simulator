eeb85871b36774b223f58bc9919cc6673438f4e9
Reckonzz <cl.sheng34@gmail.com>
2023-05-04 14:44:18 +0100

    updated matrix multiplication

--- programs/matrix_multiplication.s
+++ programs/matrix_multiplication.s
@@ -1,7 +1,8 @@
 .section .data
-A: .word 1,2,3,4,5,6,7,8,9
-B: .word 1,2,3,4,5,6,7,8,9
-C: .word 0,0,0,0,0,0,0,0,0
+A: .word 1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17
+B: .word 1,2,3,4,5,6,7,8,5,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,5,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,5,9,10,11,12,13,14,15,1,2,3,4,5,6,7,8,5,9,10,11,12,13,14,15
+C: .word 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+
 
 
 .text
@@ -13,11 +14,11 @@ _start:
     li t3, 0 
     # setting matrix A dimensions to i x k and matrix B dimensions to k x j while C dimensions are i x j
     # i 
-    li s1, 3
+    li s1, 8
     # j
-    li s2, 3
+    li s2, 8
     # k
-    li s3, 3 
+    li s3, 8
     
     la s4, A
     la s5, B
@@ -40,20 +41,20 @@ loop3:
     # t4 for A indexing, t5 for B indexing, t6 for C indexing 
     bge t3, s3, endloop3
     mul t4, t1, s1
-    add t4, t4, t3
-    add t4, t4, s4 
+    add s11, t3, s4
+    add t4, t4, s11
     lw t4, 0(t4)
 
     mul t5, t3, s3 
-    add t5, t5, t2 
-    add t5, t5, s5 
+    add s11, t2, s5 
+    add t5, t5, s11 
     lw t5, 0(t5)
 
     mul s7, t4, t5
 
     mul t6, s1, t1 
-    add t6, t6, t2
-    add t6, t6, s6 
+    add s11, t2, s6 
+    add t6, t6, s11
 
     lw s8, 0(t6)
     add s7, s8, s7
